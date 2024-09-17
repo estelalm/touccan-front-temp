8ad5e11710482e4bee7164f7ce150c81d2a40d2e
Estela Alves de Moraes <esteela.almoraes@gmail.com>
2024-09-17 08:53:02 -0300

    15 set. 2024 - login funcionando

--- telasUsuario/src/login/App.jsx
+++ telasUsuario/src/login/App.jsx
@@ -10,81 +10,94 @@ function App() {
         senha: ''
     });
 
-    // //validar erros
-    // const [erros, setErros] = useState({})
+     //validar erros
+     const [erros, setErros] = useState({})
 
-    // //validar o formulário
-    // const validateForm = () => {
-    //     const novosErros = {}
+     //validar o formulário
+     const validateForm = () => {
+         const novosErros = {}
 
-    //     // validação de e-mail
-    //     const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/
-    //     if (!emailRegex.test(formData.email)) {
-    //         novosErros.email = "E-mail inválido";
-    //     }
+         // validação de e-mail
+         const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/
+         if (!emailRegex.test(formData.email)) {
+             novosErros.email = "E-mail inválido";
+         }
 
-    //     //validação de senha
-    //     const senhaRegex = /^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
-    //     if (!senhaRegex.test(formData.senha)) {
-    //         novosErros.senha = "A senha deve ter no mínimo 8 caracteres, incluindo números, letra maiúscula e um caractere especial"
-    //     }
+        setErros(novosErros)
 
-    //     setErros(novosErros)
-
-    //     return Object.keys(novosErros).length === 0
-    // }
-
-    const handleInputChange = (e) => {
-        const { name, value } = e.target;
-    
-        setFormData({ ...formData, [name]: formattedValue });
-      };
-
-    const handleSubmit = (event) => {
+         return Object.keys(novosErros).length === 0
+     }
+      
+    const handleSubmit = async (event) => {
+      
         event.prefentDefault()
 
         console.log("Dados a serem enviados:", dados);
 
-        fetch(`${baseUrl}/login/usuario?email=${formData.email}&senha=${formData.senha}`,{
+        const response = await fetch(`${baseUrl}/login/usuario?email=${formData.email}&senha=${formData.senha}`,{
             method: 'GET'
         }).then(response => response.json())
-        .then(data => {console.log("data")})
+        .then(data => {console.log(data)})
         .catch(error => {
-            console.error("Erro:" + error)
+            console.log("Erro:" + data)
+            event.prefentDefault()
         })
+    }
+
+
+    const togglePassword = () =>{
+        const inputSenha = document.getElementById('senha')
+        inputSenha.classList.toggle('show')
+
+        if(inputSenha.classList.contains('show'))
+        inputSenha.type = 'password'
+        else
+        inputSenha.type = 'text'
     }
 
     return (
 
+
         <div>
-            <div className='container'>
-                <div className="decoracaoLaranja"></div>
-                <div className="decoracaoCinza"></div>
-                <div className="decoracaoCinza2"></div>
-                <div className="logoPrincipal">
-                    <img src='../img/logoPrincipal.svg' alt="" />
-                </div>
-            </div>
 
-            <div className='app-container'>
-                <h1>Login</h1>
-                <form onSubmit={handleSubmit}>
-                    <div className="input-container">
-                        <img src="./img/email.png" alt="" />
-                        <input type="text" name='email' placeholder='E-mail'
-                        />
-                    </div>
-                    <div className="input-container">
-                        <img src="./img/senha.png" alt="" />
-                        <input type="password" name='senha' placeholder='Senha' />
-                    </div>
-                    <button type='submit'>Entrar</button>
-                </form>
-            </div>
-
+        <div className='container'>
+          <div className="decoracaoLaranja"></div>
+          <div className="decoracaoCinza"></div>
+          <div className="decoracaoCinza2"></div>
+          <div className="logoPrincipal">
+            <img src='../img/logoPrincipal.svg' alt="" />
+          </div>
+  
         </div>
-
-
+  
+        <div className="app-container">
+          <h1>Login</h1>
+          <form onSubmit={handleSubmit}>
+            <div style={{ display: 'flex', flexDirection: 'column', gap: '46px' }}>
+                <div className='input-div'>
+                  <img src="./img/email.png" alt="Email" width={18} />
+                  <input type="text" name="email"  onChange={(e) => setFormData(e.target.value)} placeholder="E-mail" />
+                  {erros.email && <p style={{ color: 'red' }}>{erros.email}</p>}
+                </div>
+                <div className='input-div'>
+                  <img src="./img/senha.png" alt="Senha" />
+                  <input id='senha' type="password" name="senha"  onChange={(e) => setFormData(e.target.value)} placeholder="Senha" />
+                  <img src="./img/olho.png" alt="Senha"style={{width: '30px', height: '30px'}} onClick={togglePassword} />
+                </div>
+                <a href="#">Esqueceu sua senha?</a>
+                {erros.senha && <p style={{ color: 'red' }}>{erros.senha}</p>}
+            </div>
+            <button type="submit">
+              Entrar
+            </button>
+          </form>
+            <p>
+              Não tem uma conta? <br />
+              <span>Faça seu cadastro</span>
+              </p>
+            
+        </div>
+      </div>
 
     )
 }
